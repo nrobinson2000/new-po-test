eeee82f4eab217d64e71e709d8a1cbfa01867598
Nathan Robinson <nrobinson2000@me.com>
2017-04-21 02:02:51 -0400

    Initial commit at 02:02 of 2017-04-21

---
+++ ci/travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt install -y expect
+
+curl -sLO https://raw.githubusercontent.com/nrobinson2000/po-util/master/po-util.sh
+
+chmod +x po-util.sh
+
+curl -sLO https://raw.githubusercontent.com/nrobinson2000/po-util/master/config-expect.sh
+
+chmod +x config-expect.sh
+
+./po-util.sh install ~/github basic
+
+po lib clean . -f &> /dev/null
+po lib setup
+po photon build
